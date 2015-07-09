9355cdefb83b9210e1cf4d8e601e75e83f2b8251
Artem Golubev <artem.golubev@gmail.com>
2015-07-08 23:10:01 -0700

    Fix URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 AppdirectIntegration::Engine.routes.draw do
-  match 'appdirect/api/order' => 'appdirect_integration/appdirect_callbacks#order'
-  match 'appdirect/api/change' => 'appdirect_integration/appdirect_callbacks#change'
-  match 'appdirect/api/cancel' => 'appdirect_integration/appdirect_callbacks#cancel'
-  match 'appdirect/api/status' => 'appdirect_integration/appdirect_callbacks#status'
+  match '/appdirect/api/order' => 'appdirect_integration/appdirect_callbacks#order', via: [:get]
+  match '/appdirect/api/change' => 'appdirect_integration/appdirect_callbacks#change', via: [:get]
+  match '/appdirect/api/cancel' => 'appdirect_integration/appdirect_callbacks#cancel', via: [:get]
+  match '/appdirect/api/status' => 'appdirect_integration/appdirect_callbacks#status', via: [:get]
 end
