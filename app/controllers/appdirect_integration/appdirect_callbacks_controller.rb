3fc129200f8699766be75348a7745fc9bc103be4
Artem Golubev <artem.golubev@gmail.com>
2015-07-08 23:53:11 -0700

    Allways success

--- app/controllers/appdirect_integration/appdirect_callbacks_controller.rb
+++ app/controllers/appdirect_integration/appdirect_callbacks_controller.rb
@@ -2,22 +2,42 @@ module AppdirectIntegration
   class AppdirectCallbacksController < ApplicationController
     def order
       puts "order"
-      render :text => "order"
+      render :text => "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>
+<result>
+    <success>true</success>
+    <message>Account creation successful</message>
+    <accountIdentifier>new-account-identifier</accountIdentifier>
+</result>"
     end
 
     def change
       puts "change"
-      render :text => "change"
+      render :text => "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>
+<result>
+    <success>true</success>
+    <message>Account creation successful</message>
+    <accountIdentifier>new-account-identifier</accountIdentifier>
+</result>"
     end
 
     def cancel
       puts "cancel"
-      render :text => "cancel"
+      render :text => "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>
+<result>
+    <success>true</success>
+    <message>Account creation successful</message>
+    <accountIdentifier>new-account-identifier</accountIdentifier>
+</result>"
     end
 
     def status
       puts "status"
-      render :text => "status"
+      render :text => "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>
+<result>
+    <success>true</success>
+    <message>Account creation successful</message>
+    <accountIdentifier>new-account-identifier</accountIdentifier>
+</result>"
     end
   end
 end
