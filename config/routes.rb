e447ba55c779adf1f81947dfa222add52aae3844
Artem Golubev <artem.golubev@gmail.com>
2015-07-08 23:44:35 -0700

    Make routes external

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-AppdirectIntegration::Engine.routes.draw do
+Rails.application.routes.draw do
   match '/appdirect/api/order' => 'appdirect_integration/appdirect_callbacks#order', via: [:get]
   match '/appdirect/api/change' => 'appdirect_integration/appdirect_callbacks#change', via: [:get]
   match '/appdirect/api/cancel' => 'appdirect_integration/appdirect_callbacks#cancel', via: [:get]
